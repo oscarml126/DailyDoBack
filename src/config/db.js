8def0f6b61e2f6188661258ae0fef67d64a4ddbb
oscarml126 <oscarmontano222@gmail.com>
2025-03-01 15:40:14 -0500

    paso a local

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://oscardailydo:12345@134.209.222.171:5432/dailydo',
+  connectionString: process.env.DATABASE_URL || 'postgres://oscardailydo:12345@localhost:5432/dailydo',
   // Puedes agregar opciones adicionales, por ejemplo, ssl: { rejectUnauthorized: false } para producción.
 });
 
