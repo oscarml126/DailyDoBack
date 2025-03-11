1145463779eee9632a73f697d5b420224ca87b92
oscarml126 <oscarmontano222@gmail.com>
2025-03-10 22:34:40 -0500

    cambios

--- src/routes/list.routes.js
+++ src/routes/list.routes.js
@@ -1,17 +1,14 @@
-// routes/lists.routes.js
 const express = require('express');
 const router = express.Router();
-const listsController = require('../controllers/list.controller');
-const { getLists, getListById, createList,updateList,deleteList } = require('../controllers/list.controller');
+const { getLists, getListById, createList, updateList, deleteList, updateListItem, deleteListItem } = require('../controllers/lists.controller');
 
 // Endpoints para listas
 router.get('/', getLists);
-router.get('/:id',getListById);
+router.get('/:id', getListById);
 router.post('/', createList);
 router.put('/:id', updateList);
 router.delete('/:id', deleteList);
-
-// Endpoint para actualizar un item (checkbox) de una lista
-router.put('/:listId/items/:itemId', listsController.updateListItem);
+router.put('/:listId/items/:itemId', updateListItem);
+router.delete('/:listId/items/:itemId', deleteListItem);
 
 module.exports = router;
