37f89f3a97e977396d9d35a3cc43628da6a49457
oscarml126 <oscarmontano222@gmail.com>
2025-06-17 09:52:47 -0500

    cambios de node

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,25 +1,19 @@
-
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
-
-
-
-//process.env.FROM_EMAIL
+require('dotenv').config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendRecoveryEmail = async (toEmail, recoveryCode) => {
   const msg = {
     to: toEmail,
-    from: 'dailydoaplication@gmail.com', // Debe estar verificado en SendGrid
+    from: process.env.FROM_EMAIL,
     subject: 'Recuperación de Contraseña - DailyDo',
-    text: `Tu código de recuperación es: ${recoveryCode}  Este código expira en 15 minutos.`,
+    text: `Tu código de recuperación es: ${recoveryCode}. Este código expira en 15 minutos.`,
     html: `<strong>Tu código de recuperación es:</strong> <code>${recoveryCode}</code><br/>Este código expira en 15 minutos.`,
   };
 
   try {
     await sgMail.send(msg);
-    console.log("Correo de recuperación enviado correctamente");
   } catch (error) {
-    console.error("Error al enviar correo:", error.response?.body || error);
     throw new Error("No se pudo enviar el correo");
   }
 };
