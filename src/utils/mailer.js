390f139df800a2cd5e9afef437f1b106a27756f5
oscarml126 <oscarmontano222@gmail.com>
2025-06-17 09:14:37 -0500

    aplicacion de sendgrid

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,25 +1,28 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
-const transporter = nodemailer.createTransport({
-     host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // STARTTLS
-  auth: {
-    user: 'oscarmontano222@gmail.com',
-    pass: 'xnqh zfdx eaba tsxn'
-    }
-});
+
 
 //process.env.FROM_EMAIL
 
-exports.sendRecoveryEmail = async (toEmail, recoveryCode) => {
-    const mailOptions = {
-        from: "oscarmontano222@gmail.com", 
-        to: toEmail,
-        subject: 'Recuperación de Contraseña',
-        text: `Tu código de recuperación es: ${recoveryCode}. Este código es válido por 15 minutos.`
-    };
+const sendRecoveryEmail = async (toEmail, recoveryCode) => {
+  const msg = {
+    to: toEmail,
+    from: 'dailydoaplication@gmail.com', // Debe estar verificado en SendGrid
+    subject: 'Recuperación de Contraseña - DailyDo',
+    text: `Tu código de recuperación es: ${recoveryCode}  Este código expira en 15 minutos.`,
+    html: `<strong>Tu código de recuperación es:</strong> <code>${recoveryCode}</code><br/>Este código expira en 15 minutos.`,
+  };
 
-    return transporter.sendMail(mailOptions);
+  try {
+    await sgMail.send(msg);
+    console.log("Correo de recuperación enviado correctamente");
+  } catch (error) {
+    console.error("Error al enviar correo:", error.response?.body || error);
+    throw new Error("No se pudo enviar el correo");
+  }
 };
+
+module.exports = { sendRecoveryEmail };
